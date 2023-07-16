5f7a404e1bf5d8ee38e4e3f7e4cf38fa53e1deee
Mehmet Nail Mutlu <54910442+mehmetnail0@users.noreply.github.com>
2023-07-17 00:55:44 +0300

    changed
    changed html files. Added things that look website great. added js commands. added some images

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,24 @@
-function goLeft() {
-    console.log('left')
+const carousel = document.querySelector('.projectCards')
+const card = document.querySelector('.projectCard')
+const leftButton = document.querySelector('#goLeftArrow')
+const rightButton = document.querySelector('#goRightArrow')
+
+const carouselWidth = carousel.offsetWidth;
+const cardStyle = card.currentStyle || window.getComputedStyle(card)
+const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g));
+
+const cardCount = carousel.querySelectorAll('.projectCard').length
+
+
+// console.log(maxX)
+
+rightButton.onclick = function () {
+    const widthItem = card.offsetWidth;
+    carousel.scrollLeft += widthItem; console.log('sadsadsda')
+
 }
-
-
-function goRight() {
-    console.log('right')
+leftButton.onclick = function () {
+    const widthItem = card.offsetWidth;
+    carousel.scrollLeft -= widthItem;
+    console.log('sadsadsda')
 }
-
